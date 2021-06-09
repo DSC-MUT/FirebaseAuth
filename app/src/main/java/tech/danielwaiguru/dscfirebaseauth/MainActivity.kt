0c6a6fbcf0d6ac30a85b10e8028ef164a822da5e
Daniel Waiguru <danielirungu91@gmail.com>
2021-06-09 07:30:18 +0300

    Initial commit

---
+++ app/src/main/java/tech/danielwaiguru/dscfirebaseauth/MainActivity.kt
@@ -0,0 +1,14 @@
+package tech.danielwaiguru.dscfirebaseauth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.danielwaiguru.dscfirebaseauth.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
